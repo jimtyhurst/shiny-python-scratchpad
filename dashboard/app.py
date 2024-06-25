e5f695e5b7663cac442c10c0e2d98a0d5f64c55a
Jim Tyhurst <jim@tyhurst.com>
2024-06-25 16:30:07 -0700

    Displays cards in columns.

--- dashboard/app.py
+++ dashboard/app.py
@@ -12,22 +12,23 @@ ui.h1("My Penguin Dashboard")
 with ui.sidebar(bg="#f8f8f8"):
     ui.input_slider(id='mass', label='Maximum body mass (grams) to display', min=2000, max=8000, value=6000)
 
-with ui.card():
-    'Bill dimensions by species'
-    
-    @render_plotly
-    def plot():
-        df_subset = df[df['body_mass_g'] < input.mass()]
-        return px.scatter(
-            df_subset, 
-            x='bill_depth_mm', 
-            y='bill_length_mm',
-            color='species',
-        )
+with ui.layout_columns():
+    with ui.card():
+        'Bill dimensions by species'
 
-with ui.card():
-    'Raw Data'
+        @render_plotly
+        def plot():
+            df_subset = df[df['body_mass_g'] < input.mass()]
+            if input.show_species():
+                return px.scatter(df_subset, x='bill_depth_mm', y='bill_length_mm', color='species')
+            else:
+                return px.scatter(df_subset, x='bill_depth_mm', y='bill_length_mm')
+        
+        ui.input_checkbox('show_species', 'Show Species', value=True)
 
-    @render.data_frame
-    def data():
-        return df[df['body_mass_g'] < input.mass()]
+    with ui.card():
+        'Raw Data'
+
+        @render.data_frame
+        def data():
+            return df[df['body_mass_g'] < input.mass()]
